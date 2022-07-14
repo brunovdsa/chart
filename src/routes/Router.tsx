89abf274fc8cd41509cbec55fb06e08648b0d949
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2022-07-14 14:54:30 -0300

    first commit

---
+++ src/routes/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { About } from '../pages/about';
+import { Home } from '../pages/home';
+import { NotFound } from '../pages/notFound';
+
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/income-statement' element={<About />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default Router;
